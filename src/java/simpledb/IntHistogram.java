ad456291709a2dc402be6aad7432570f0d38ddf2
Shawn Lin <shawn3298317lin@gmail.com>
2020-11-25 15:16:42 -0500

    Fix historgram bug & Pass Ex.1 + Ex.2

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -4,6 +4,7 @@ import static com.sun.javafx.util.Utils.sum;
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
 import static java.lang.Math.abs;
+import static java.lang.Math.ceil;
 
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
@@ -14,6 +15,7 @@ public class IntHistogram {
     private int _max;
     private int _n_tuples;
     private double[] _hist_freq;
+    private double _wb;
 
     /**
      * Create a new IntHistogram.
@@ -38,6 +40,7 @@ public class IntHistogram {
         this._max = max;
         this._n_tuples = 0;
         this._hist_freq = new double[_n_buckets];
+        this._wb = ((double)_max - _min) / _n_buckets;
     }
 
     /**
@@ -69,20 +72,27 @@ public class IntHistogram {
         if (op == Predicate.Op.EQUALS) {
             if (v < _min || v > _max) return 0.0;
             int bucket = getBucket(v);
-            return _hist_freq[bucket]/_n_tuples;
+            // Debug.log("EQUAL: bucket[%d] v[%d] freq[%f]/%f/%d", bucket, v, _hist_freq[bucket], _wb, _n_tuples);
+            return _hist_freq[bucket]/ceil(_wb)/_n_tuples;
         } else if (op == Predicate.Op.NOT_EQUALS) {
             if (v < _min || v > _max) return 1.0;
             int bucket = getBucket(v);
-            return (1.0 - _hist_freq[bucket]/_n_tuples);
+            return (1.0 - _hist_freq[bucket]/ceil(_wb)/_n_tuples);
         }
         else if (op == Predicate.Op.GREATER_THAN || op == Predicate.Op.GREATER_THAN_OR_EQ) {
 
-            v = max(min(_max, v), _min);
+            // v = max(min(_max, v), _min);
+            if (v > _max) return 0.0;
+            if (v < _min) return 1.0;
             int bucket = getBucket(v);
-            double w_b = ((double)_max - _min) / _n_buckets;
-            double b_right = _min + (bucket+1) * w_b;
-            double acum_selectivity = (b_right - v) / w_b * _hist_freq[bucket];
-//            Debug.log("bucket: %d, w_b: %f, b_right: %f; v = %d; selectivity: %f", bucket, w_b, b_right, v, acum_selectivity);
+            double b_right = _min + (bucket+1) * _wb;
+            double acum_selectivity = (b_right - v) / ceil(_wb) * _hist_freq[bucket];
+            //Debug.log("v: %d; bucket: %d; [%d, %d]/%d", v, bucket, _min, _max, _n_buckets);
+            //Debug.log("bucket: %d, w_b: %f, b_right: %f; v = %d; selectivity: %f", bucket, _wb, b_right, v, acum_selectivity);
+
+            if (op == Predicate.Op.GREATER_THAN_OR_EQ)
+                acum_selectivity += (_hist_freq[bucket] / ceil(_wb));
+
             for (int i = bucket+1; i < _n_buckets; i++) {
                 acum_selectivity += (_hist_freq[i]);
             }
@@ -91,18 +101,24 @@ public class IntHistogram {
 
         } else if (op == Predicate.Op.LESS_THAN || op == Predicate.Op.LESS_THAN_OR_EQ) {
 
+            if (v > _max) return 1.0;
+            if (v < _min) return 0.0;
+
             v = max(min(_max, v), _min);
             int bucket = getBucket(v);
-            double w_b = ((double)_max - _min) / _n_buckets;
-            double b_left = _min + bucket * w_b;
-            double acum_selectivity = (v - b_left) / w_b * _hist_freq[bucket];
-            Debug.log("bucket: %d, w_b: %f, b_right: %f; v = %d; (%f); selectivity: %f", bucket, w_b, b_left, v, _hist_freq[bucket], acum_selectivity);
+            double b_left = _min + bucket * _wb;
+            double acum_selectivity = (v - b_left) / ceil(_wb) * _hist_freq[bucket];
+            // Debug.log("bucket: %d, w_b: %f, b_right: %f; v = %d; (%f); selectivity: %f", bucket, _wb, b_left, v, _hist_freq[bucket], acum_selectivity);
+
+            if (op == Predicate.Op.LESS_THAN_OR_EQ)
+                acum_selectivity += (_hist_freq[bucket] / ceil(_wb));
+
             for (int i = bucket-1; i >= 0; i--) {
                 acum_selectivity += (_hist_freq[i]);
-                Debug.log("[%d] + %f => %f", i, _hist_freq[i], acum_selectivity);
+                // Debug.log("[%d] + %f => %f", i, _hist_freq[i], acum_selectivity);
             }
             acum_selectivity /= _n_tuples;
-            Debug.log("Final select: %f", acum_selectivity);
+            // Debug.log("Final select: %f", acum_selectivity);
             return acum_selectivity;
         }
 
@@ -123,12 +139,13 @@ public class IntHistogram {
         // some code goes here
 
         // selectivity = avg(sum(selectivity for each bucket))
-        return sum(_hist_freq)/_n_buckets/_n_tuples;
+        return sum(_hist_freq)/_wb/_n_tuples;
         // return 1.0;
     }
 
     private int getBucket(int v) {
         int bucket = (int) (((double) v - _min) / (_max - _min) * _n_buckets);
+//        int bucket = (int) (v/_wb);
         return min(_n_buckets-1, bucket);
     }
     
