a38c4101841114a1e16b9fd442d01bd05cf182cd
Shawn Lin <shawn3298317lin@gmail.com>
2020-12-01 19:21:24 -0500

    Update test cases

--- test/simpledb/StringHistogramTest.java
+++ test/simpledb/StringHistogramTest.java
@@ -46,76 +46,74 @@ public class StringHistogramTest {
 	 * (or, at least, reasonably small space; O(log(n)) might still work if
 	 * your constants are good).
 	 */
-	@Test public void orderOfGrowthTest() {
+	@Test public void opLikeTest() {
 		// Don't bother with a timeout on this test.
 		// Printing debugging statements takes >> time than some inefficient algorithms.
-		StringHistogram h = new StringHistogram(200000000);
+		SuffixTreeStringHistogram h = new SuffixTreeStringHistogram();
 		String alphaNumericChars = "abcdefghijklmnopqrstuvwxyz";
 
 		// Feed the histogram more alphabetic string than would fit into our
 		// 128mb allocated heap (4-byte string)
 		// If this fails, someone's storing every value...
-//		for (int c = 0; c < 33554432; c++) { // should be 134 MB
-		for (int c = 0; c < 100000; c++) { // should be 134 MB
-			String uuid = generateRandomChars(alphaNumericChars, 5);
+		// "a" string fields
+		for (int c = 0; c < 16554432; c++) { // should be 134 MB
+			String uuid = "a" + generateRandomChars(alphaNumericChars.substring(1), 4);
 			h.addValue(uuid);	// Pseudo-random number; at least get a distribution
 		}
-		
-		// Try printing out all of the values; make sure "estimateSelectivity()"
-		// cause any problems
-		double selectivity = 0.0;
 
-		ArrayList<String> combinations = new ArrayList<>();
-		combinations.add("");
-		for (int i = 4; i > 0; i--)
-			printAllKLength(alphaNumericChars.toCharArray(), i, combinations);
-		Debug.log("res = %d", combinations.size());
-
-		for (int c = 0; c < combinations.size(); c++) {
-			String wildcard_query = combinations.get(c);
-			selectivity += h.estimateSelectivity(Op.EQUALS, wildcard_query);
-			if ((c % 1000) == 1)
-				Debug.log("Current query = %s, selectivity = %f", wildcard_query, selectivity);
+		// No-"a" string fields
+		for (int c = 0; c < 16554432; c++) {
+			String uuid = generateRandomChars(alphaNumericChars.substring(1), 5);
+			h.addValue(uuid);	// Pseudo-random number; at least get a distribution
 		}
-		selectivity /= 100000;
-		Debug.log("selectivity = %f", selectivity);
 
-		// All the selectivities should add up to 1, by definition.
-		// Allow considerable leeway for rounding error, though 
-		// (Java double's are good to 15 or so significant figures)
-		Assert.assertTrue(selectivity > 0.99);
+		Assert.assertTrue(h.estimateSelectivity(Op.LIKE, "a") == 0.5); // LIKE *a* -> 0.5
 	}
 
-	/**
-	 * Make sure that LIKE binning does something reasonable.
-	 */
-	@Test public void opLikeTest() {
-		StringHistogram h = new StringHistogram(10);
+	@Test public void opLikeTestWithoutSuffixTree() {
+		// Don't bother with a timeout on this test.
+		// Printing debugging statements takes >> time than some inefficient algorithms.
+		StringHistogram h = new StringHistogram(1000000);
+		String alphaNumericChars = "abcdefghijklmnopqrstuvwxyz";
 
-//		// Set some values
-//		h.addValue(3);
-//		h.addValue(3);
-//		h.addValue(3);
-//		h.addValue(1);
-//		h.addValue(10);
-//
-//		// Be conservative in case of alternate implementations
-//		Assert.assertTrue(h.estimateSelectivity(Op.GREATER_THAN, -1) > 0.999);
-//		Assert.assertTrue(h.estimateSelectivity(Op.GREATER_THAN, 2) > 0.6);
-//		Assert.assertTrue(h.estimateSelectivity(Op.GREATER_THAN, 4) < 0.4);
-//		Assert.assertTrue(h.estimateSelectivity(Op.GREATER_THAN, 12) < 0.001);
+		// Feed the histogram more alphabetic string than would fit into our
+		// 128mb allocated heap (4-byte string)
+		// If this fails, someone's storing every value...
+		// "a" string fields
+		for (int c = 0; c < 16554432; c++) { // should be 134 MB
+			String uuid = "a" + generateRandomChars(alphaNumericChars.substring(1), 4);
+			h.addValue(uuid);	// Pseudo-random number; at least get a distribution
+		}
+
+		// No-"a" string fields
+		for (int c = 0; c < 16554432; c++) {
+			String uuid = generateRandomChars(alphaNumericChars.substring(1), 5);
+			h.addValue(uuid);	// Pseudo-random number; at least get a distribution
+		}
+
+		double selectivity = h.estimateSelectivity(Op.LIKE, "a");
+		Debug.log("selectivity = %.2f", selectivity);
+		Assert.assertTrue(selectivity < 0.1); // Answer should be 0.5
 	}
 
-	/**
-	 * Test
-	 */
+
+
 	@Test public void suffixTreeTest() {
-		String bookkeeper = "bookkeeper";
 		SuffixTree tree = new SuffixTree("greener");
+
+		for (int i = 0; i < 100; i++)
+			tree.insertString("grey");
+		for (int i = 0; i < 99; i++)
+			tree.insertString("greener");
+		for (int i = 0; i < 100; i++)
+			tree.insertString("grew");
 		Debug.log("%s", tree.printTree());
-		tree.insertString("ener");
-//		tree.addSuffix();
-		Debug.log("%s", tree.printTree());
+
+		Assert.assertTrue(tree.searchText("gre") == 300.0);
+		Assert.assertTrue(tree.searchText("ey") == 100.0);
+		Assert.assertTrue(tree.searchText("n") == 100.0);
+		Assert.assertTrue(tree.searchText("g") == 300.0);
+		Assert.assertTrue(tree.searchText("") == 300.0);
 
 	}
 }
